abffa74d0616cddb36bc752399f9634cb951ed74
justin <justin.j@neoito.com>
2024-09-10 23:26:04 -0500

    image issue

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ["localhost"],
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "cdn.sanity.io",
-          port: "",
-        },
-      ],
-    },
-    experimental:{
-      images:{
-        allowFutureImage: true
-      }
-    }
+    // images: {
+    //   domains: ["localhost"],
+    //   remotePatterns: [
+    //     {
+    //       protocol: "http",
+    //       hostname: "cdn.sanity.io",
+    //       port: "",
+    //     },
+    //   ],
+    // },
+    // experimental:{
+    //   images:{
+    //     allowFutureImage: true
+    //   }
+    // }
   };
   
   module.exports = nextConfig;
